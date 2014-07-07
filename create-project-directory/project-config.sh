14a0a65d1c318f39dd9362e1700aab18f3b6d96f
Dev--xu <xushuangshuang1@gmail.com>
2014-07-07 10:06:36 +0800

    增加了对项目导入时将无关的代码删除并且提交

--- create-project-directory/project-config.sh
+++ create-project-directory/project-config.sh
@@ -11,3 +11,17 @@ echo "PROJRCT  AND CONFIG  SUCCESS"
 cp /d/progrem/projectTools/project-script/* .
 
 echo "SCRIPT SUCCESS"
+
+git add .
+
+git commit -m "初始化仓库并且将测试用到的所有jar文件全部倒入项目"
+
+rm config-test.sh
+
+rm create-project-directory.sh
+
+rm project-config.sh
+
+git rm .
+
+git commit -m "删除和项目无关的代码"
