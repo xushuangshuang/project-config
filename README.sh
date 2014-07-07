8a1d3ce5249e2bcd290e86ccfbb79a69ae7336d7
Dev--xu <xushuangshuang1@gmail.com>
2014-07-07 09:35:56 +0800

    添加了README文件，便于以后的更新

---
+++ README.sh
@@ -0,0 +1,18 @@
+这个是自己使用的项目前期配置文件，需要和git结合使用
+
+project-script
+	|
+	|-- clean.sh   项目的清空脚本
+	|
+	|-- compile.sh  项目编译脚本（此脚本中的源文件地址还需要补全）
+	|
+	|-- run-test.sh  执行测试脚本（此脚本中需要导入有main方法的类名）
+
+create-project-directory
+	|
+	|-- create-project-directory.sh  （此脚本是执行构建项目目录）
+	|
+	|-- config-test.sh  (此脚本是配置test时的需要导入大量的jar文件)
+	|
+	|-- project-config.sh (此脚本是用来执行前面两个命令，并且初始化仓库)
+
